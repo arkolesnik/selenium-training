3267ae1fe4fcf8d0a1f280b4f56b3c83725d81ea
Olena Kolesnyk <a.r.kolesnik@gmail.com>
2017-06-13 20:36:36 +0300

    Fixes

--- sel7/src/test/java/CartTest.java
+++ sel7/src/test/java/CartTest.java
@@ -7,13 +7,19 @@ public class CartTest extends DriverFixture {
     @Test
     public void addProductToCart() {
         driver.get("http://localhost/litecart/en/");
-        By productSelector = By.xpath(".//div[@id='box-most-popular']//li[1]");
+
+        int numberOfProductInList = 1;
         By actualProductNumberSelector = By.cssSelector("#cart a.content span.quantity");
+        By sizeSelectSelector = By.cssSelector("select[name='options[Size]']");
 
         for (int i = 1; i < 4; i++) {
-            driver.findElement(productSelector).click();
+            driver.findElements(By.xpath(".//div[@id='box-most-popular']//li")).get(numberOfProductInList - 1).click();
 
             String previousNumber = driver.findElement(actualProductNumberSelector).getText();
+            if (driver.findElements(sizeSelectSelector).size() == 1) {
+                driver.findElement(sizeSelectSelector).click();
+                driver.findElement(By.xpath(".//option[text()='Small']")).click();
+            }
             driver.findElement(By.cssSelector("button[name='add_cart_product']")).click();
             Integer expectedNumber = Integer.valueOf(previousNumber) + 1;
             wait.until(ExpectedConditions.textToBe(actualProductNumberSelector, expectedNumber.toString()));
